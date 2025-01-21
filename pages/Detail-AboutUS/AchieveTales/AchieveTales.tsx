035719c30b1f7869f52dee862888f3cfc582b0fc
Muzamilkhan110 <muzamilmagsi546@gmail.com>
2025-01-21 11:37:09 +0500

    Achievement section added in AboutUs page

--- pages/Detail-AboutUS/AchieveTales/AchieveTales.tsx
+++ pages/Detail-AboutUS/AchieveTales/AchieveTales.tsx
@@ -11,14 +11,14 @@ const AchieveTales = () => {
            approaches." />
      </div>
           <div className="flex flex-col md:flex-row justify-center pt-[5%] px-[2%] xl:px-[8%]">
-          <div className="w-full md:w-1/2 border-b pb-8 md:pb-0 md:border-r"> 
-             <div className="flex justify-center items-center gap-1">
-             <Image src={'/journey.svg'}  alt="" width={45} height={25}/>
-             <Heading tittle="From Frustration to" />
-             <Heading dynamic="Success"/>
+          <div className="w-full md:w-1/2  pb-8 md:pb-0 md:border-r"> 
+             <div className="flex justify-center items-center gap-2">
+             <Image className=" w-8 lg:w-10" src={'/journey.svg'}  alt="" width={45} height={25}/>
+             <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl  text-white font-bold">From Frustration to</h1>
+             <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl useclass font-bold">Success</h1>
              </div>
              <div className="flex flex-col text-left px-4 md:px-[9%]">
-             <p className="pt-3 text-stone-400 mt-2 text-sm sm:text-base md:text-lg">In 2015, a young boy, frustrated by his father's automobile business challenges, discovered a market gap in digital transitions for brick-and-mortar businesses. His successful digital marketing project for his father’s business inspired him to co-found ConceptRecall with a tech-savvy partner. Starting as a small digital agency, it has grown to over 100 employees, dedicated to digitizing businesses.
+             <p className="pt-3 text-stone-400 mt-2 text-sm md:text-base lg:text-lg">In 2015, a young boy, frustrated by his father's automobile business challenges, discovered a market gap in digital transitions for brick-and-mortar businesses. His successful digital marketing project for his father’s business inspired him to co-found ConceptRecall with a tech-savvy partner. Starting as a small digital agency, it has grown to over 100 employees, dedicated to digitizing businesses.
             <br/>
             <br/>
              By 2024, ConceptRecall has served more than 2,500 clients worldwide, delivered over 10,000 projects, and achieved annual revenues exceeding $100 million. Today, it stands as a testament to innovation born from frustration and a commitment to sustainable digital growth.</p> 
@@ -26,22 +26,23 @@ const AchieveTales = () => {
              </div>
           </div>
           <div className="w-full md:w-1/2 px-4 md:px-[4%]">
-            <p className="py-3 text-stone-400 mt-2 text-sm sm:text-base md:text-lg">Throughout our history, we've driven many companies towards prosperity, fostering strong relationships with a teamwork-driven mindset.
+            <hr className="md:hidden" />
+            <p className="py-4 text-stone-400 mt-2 text-sm md:text-base lg:text-lg">Throughout our history, we've driven many companies towards prosperity, fostering strong relationships with a teamwork-driven mindset.
             <br/>
             <br/>
             We take pride in aiding diverse businesses, from emerging startups to seasoned companies, to flourish and overcome obstacles through customized strategies that help them achieve their goals and bring their dreams to life</p>
             <hr className="border-gray-400 " />
-            <div className="flex gap-2 justify-center items-center">
+            <div className="flex flex-col md:flex-row gap-2 md:justify-center md:items-center py-6">
             <div className="w-[65%]">
                  <h1 className="useclass text-5xl font-bold">250+</h1>
                  <p className="text-white">Businesses Flourished</p>
             </div>
             <div className="">
-               <p className="py-3 text-stone-400 mt-2 text-sm sm:text-base md:text-lg">We've guided over 250 businesses towards achieving their goals through our creative strategies.</p>
+               <p className="py-3 text-stone-400 mt-2 text-sm md:text-base lg:text-lg">We've guided over 250 businesses towards achieving their goals through our creative strategies.</p>
             </div>
             </div>
             <hr className="border-gray-400 " />
-            <div className="flex gap-2 justify-center items-center">
+            <div className="flex flex-col md:flex-row gap-2 md:justify-center md:items-center py-6">
             <div className="w-[95%]">
                  <h1 className="useclass text-5xl font-bold">$1B+</h1>
                  <p className="text-white">Surpassed $1 Billion</p>
