4b398f7350094fb4ea54a2260b6540ca97f7601c
Muzamilkhan110 <muzamilmagsi546@gmail.com>
2025-01-30 18:09:49 +0500

    Dynamic Page

--- pages/Detail-Sevices/OurApproach.tsx
+++ pages/Detail-Sevices/OurApproach.tsx
@@ -6,36 +6,64 @@ import { useState, useEffect } from "react";
 import CompIndus from "@/components/CompIndus/CompIndus";
 import { imgData } from "./WeServe";
 
+// const OurApproach = () => {
+//   return (
+//     <div>
+//       <div>
+//         {ourData.filter((member) => member.id % 2 === 1).map((data, index) => (
+//           <div key={index} className="">
+//             <ServiceComponent
+//               heading1={data.heading1}
+//               heading2={data.heading2}
+//               paragraph={data.paragraph}
+//               isBlackBackground={true}
+//             />
+//           </div>
+//         ))}
+//         {ourData.filter((member) => member.id % 2 === 0).map((data, index) => (
+//           <div key={index} className="">
+//             <ServiceComponent
+//               id={2}
+//               heading1={data.heading1}
+//               heading2={data.heading2}
+//               paragraph={data.paragraph}
+//               heading3={data.heading3}
+//               heading4={data.heading4}
+//               paragraph1={data.paragraph1}
+//               isBlackBackground={!true}
+//             />
+            
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default OurApproach;
+
+
 const OurApproach = () => {
   return (
     <div>
-      <div>
-        {ourData.filter((member) => member.id % 2 === 1).map((data, index) => (
-          <div key={index} className="">
-            <ServiceComponent
-              heading1={data.heading1}
-              heading2={data.heading2}
-              paragraph={data.paragraph}
-              isBlackBackground={true}
-            />
-          </div>
-        ))}
-        {ourData.filter((member) => member.id % 2 === 0).map((data, index) => (
-          <div key={index} className="">
-            <ServiceComponent
-              id={2}
-              heading1={data.heading1}
-              heading2={data.heading2}
-              paragraph={data.paragraph}
-              heading3={data.heading3}
-              heading4={data.heading4}
-              paragraph1={data.paragraph1}
-              isBlackBackground={!true}
-            />
-            
-          </div>
-        ))}
-      </div>
+      {ourData.map((data, index) => {
+        const isBlackBackground = data.id % 2 === 1;
+
+        return (
+          <ServiceComponent
+            key={index}
+            heading1={data.heading1}
+            heading2={data.heading2}
+            paragraph={data.paragraph}
+            isBlackBackground={isBlackBackground}
+            {...(!isBlackBackground && { // Only pass these props when background is NOT black
+              heading3: data.heading3,
+              heading4: data.heading4,
+              paragraph1: data.paragraph1
+            })}
+          />
+        );
+      })}
     </div>
   );
 };
